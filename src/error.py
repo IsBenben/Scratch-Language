75f5c1c9da323a09c4a8975ca34dcda85f59e23a
IsBenben <542863140@qq.com>
2024-10-06 14:29:57 +0800

    for loop & new command line

--- src/error.py
+++ src/error.py
@@ -6,6 +6,9 @@ class Error:
     type: str
     msg: str
 
+class ScratchLanguageError(Exception):
+    pass
+
 def raise_error(error: Error) -> NoReturn:
     print('[ERROR!] {}: {}'.format(error.type, error.msg))
-    raise Exception(error.msg)
+    raise ScratchLanguageError(error.msg)
